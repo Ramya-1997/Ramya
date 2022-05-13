2d5a26004f61e72d036379ce8c8573a216b3754f
ramya-207 <ramyashree201997@gmail.com>
2022-05-13 17:45:43 +0530

    worked on PrintTable.java!

---
+++ ReverseNumber.java
@@ -0,0 +1,17 @@
+class ReverseNumber
+{
+public static void main(string[] args)
+{
+ int n=0;
+ int i=0;j=0;
+ for(i=0;i<=n;i++){
+  System.out.println(" ",i);
+}
+for(j=9;j<=0;j++){
+ System.out.println(" ",j)
+}
+
+   
+
+
+
